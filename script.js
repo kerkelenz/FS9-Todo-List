f0e4453f18540da7e8b63a27dff872a433582678
Kevin P. Erkelenz <kevinerkelenz@gmail.com>
2026-03-22 16:23:03 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Your Todo List App implementation will go here!
+
+// grab the form - start from chrome dev tools to do this
+const todoFormEl = document.querySelector("#todo-form");
+// grab the todo list
+// grab the error message
+
